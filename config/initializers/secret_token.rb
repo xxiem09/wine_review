b9e2d465c85931451ede5876274bfe9312bdd1f5
xxiem09 <xxiem09@yahoo.com>
2014-09-30 11:36:29 -0400

    Start my homework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WineReview::Application.config.secret_key_base = '80e1c0482b5e54ebe647c3bdbd60c9a4eb06834feff30672218a20c79596e34a71fb7b41cd71da0dfccf6fbcc22d4be70a38ac0733dbe23ea18497156de78bb0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WineReview::Application.config.session_store :cookie_store, key: '_wine_review_session'
