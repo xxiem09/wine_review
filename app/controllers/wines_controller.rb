2fa283469dde65bf76d7de70e8b140af46d79d8c
xxiem09 <xxiem09@yahoo.com>
2014-10-03 17:49:51 -0400

    homework_2

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,9 +1,45 @@
 class WinesController <ApplicationController
+  before_action :set_wine, only: [ :show, :edit, :update, :destroy ]
   def index
   	@available_at = Time.now
   	@wines = Wine.all
   end
   def show
-  	@wine = Wine.find(params[:id])
+  	
+  end
+  def new
+  	@wine = Wine.new
+  end
+  def create
+  	@wine = Wine.new(wine_params)
+  	@wine.save
+  	redirect_to @wine
+  	
+  end
+  def edit
+    
+  end
+  def update
+    
+    @wine.update(wine_params)
+    redirect_to @wine
+  end 
+  def destroy
+    
+    @wine.destroy
+    redirect_to wines_url
+  end
+
+  private
+
+  def wine_params
+  	params.require(:wine).permit(:name, :year, :winery, :country, :varietal)
+  	
+  end
+
+  private
+
+  def set_wine
+    @wine = Wine.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 WineReview::Application.routes.draw do
-  get "/wines" => "wines#index", as: 'wines'
-  get "/wines/:id"  => "wines#show", as: 'wine'
+  resources :wines
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
